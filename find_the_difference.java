97d541cd0e613c1b907d38b3dc66ad08a1c15e60
Deven Rawat <125259504+devenrawat-bit@users.noreply.github.com>
2025-05-06 12:23:35 +0530

    Add files via upload

---
+++ find_the_difference.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+//array package include the sort methods
+import java.util.Scanner;
+public class find_the_difference{
+    public static char findTheDifference(String s, String t) {
+        char s1[]=s.toCharArray();
+        char t1[]=t.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(t1);
+        //now loop for the smaller one as if second char array remain we get our answer
+        for(int i=0;i<s1.length;i++)
+        {
+           if(s1[i]!=t1[i])
+           {
+             return t1[i];
+           }
+        }
+        return t1[t1.length-1];
+   }
+    public static void main(String[] args) {
+        System.out.println("enter the first string that is less than the next string you will enter: ");
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.println("enter the second string which is greater than the first one: ");
+        String b=sc.nextLine();
+        char ans=findTheDifference(a, b);
+        System.out.println("the difference is :"+ans);
+        sc.close();
+    }
+}

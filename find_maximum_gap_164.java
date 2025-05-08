3c7e2b1ce7fdb12c2cf6183282d0b8f5d557b2ea
Deven Rawat <125259504+devenrawat-bit@users.noreply.github.com>
2025-05-08 10:43:20 +0530

    Add files via upload

---
+++ find_maximum_gap_164.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class find_maximum_gap_164 {
+    public static int maximumGap(int[] nums) {
+        if (nums.length == 1) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        // 1 3 6 9
+        int ans = 0;
+        for (int i = 1; i < nums.length; i++) {
+            int temp = nums[i] - nums[i - 1];
+            if (ans < temp)
+                ans = temp;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("enter the length of the array: ");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int arr[] = new int[a];
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("enter the %d element: " , i + 1);
+            arr[i] = sc.nextInt();
+        }
+        int ans = maximumGap(arr);
+        System.out.printf("the maximum gap in the array is: %d", ans);
+        sc.close();
+    }
+    //note this code has time complexity of this solution is nlogn and the space complexity is o(n);
+}

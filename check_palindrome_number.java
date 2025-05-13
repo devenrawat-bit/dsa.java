968c1c1459eb0a891d3b2894004db7f2d9009165
Deven Rawat <125259504+devenrawat-bit@users.noreply.github.com>
2025-05-13 20:43:52 +0530

    Add files via upload

---
+++ check_palindrome_number.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class check_palindrome_number {
+    public static boolean isPalindrome(int x) {
+        // edge case negative number are not palindrome
+        if (x < 0) {
+            return false;
+        } else {
+            int rev = 0;
+            int n = x;
+            while (n > 0) {
+                // 121
+                rev = (rev * 10) + n % 10;
+                n = n / 10;
+            }
+            return rev == x;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int a = sc.nextInt();
+        boolean ans = isPalindrome(a);
+        System.out.println(ans);
+        sc.close();
+    }
+}

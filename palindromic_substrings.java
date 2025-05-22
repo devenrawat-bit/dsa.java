287959c2ed6e2c41fdfff5168db3a1165c175372
Deven Rawat <125259504+devenrawat-bit@users.noreply.github.com>
2025-05-22 12:53:02 +0530

    Add files via upload

---
+++ palindromic_substrings.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class palindromic_substrings {
+    public static boolean isPalindrome(String s) {
+        int j = s.length() - 1;
+        int i = 0;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static int countSubstrings(String s) {
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                if (isPalindrome(s.substring(i, j)) == true) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the string: ");
+        String a = sc.nextLine();
+        int ans = countSubstrings(a);
+        System.out.println("the total number of palindromic substrings are: " + ans);
+        sc.close();
+    }
+}

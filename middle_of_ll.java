f9b1d495f7576705a0cfd310b4793e09eb9556bc
Deven Rawat <125259504+devenrawat-bit@users.noreply.github.com>
2025-05-31 09:24:06 +0530

    Add files via upload

---
+++ middle_of_ll.java
@@ -0,0 +1,40 @@
+public class middle_of_ll {
+
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    // Solution class
+    public static class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode slow = head;
+            ListNode fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        Solution sol = new Solution();
+        ListNode middle = sol.middleNode(head);
+
+        System.out.println("Middle node value: " + middle.val);
+    }
+}

3bb547a7c7b73cc0ed7376feb9be65a9a9534a4c
Deven Rawat <125259504+devenrawat-bit@users.noreply.github.com>
2025-06-03 20:15:19 +0530

    Add files via upload

---
+++ ReverseLinkedList.java
@@ -0,0 +1,55 @@
+public class ReverseLinkedList {
+
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    // Function to reverse the linked list
+    public static ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode previous = null;
+        ListNode next1 = null;
+
+        while (curr != null) {
+            next1 = curr.next;
+            curr.next = previous;
+            previous = curr;
+            curr = next1;
+        }
+
+        return previous;
+    }
+
+    // Helper function to print linked list
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        // Creating sample linked list: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        ListNode reversedHead = reverseList(head);
+
+        System.out.println("Reversed List:");
+        printList(reversedHead);
+    }
+}

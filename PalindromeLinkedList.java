30ceb0634160b7080d00554e5cadd64d77f02463
Deven Rawat <125259504+devenrawat-bit@users.noreply.github.com>
2025-06-04 22:41:58 +0530

    Add files via upload

---
+++ PalindromeLinkedList.java
@@ -0,0 +1,70 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class PalindromeLinkedList {
+
+    public static ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverse(slow.next);
+        slow.next = secondHalf;
+
+        ListNode a1 = head;
+        ListNode a2 = secondHalf;
+
+        while (a2 != null) {
+            if (a1.val != a2.val)
+                return false;
+            a1 = a1.next;
+            a2 = a2.next;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // Example: 1 -> 2 -> 2 -> 1
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+
+        boolean result = isPalindrome(head);
+        System.out.println("Is Palindrome? " + result);
+    }
+}
+
